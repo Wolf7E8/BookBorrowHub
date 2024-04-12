039c6068b4611be693cc62fbcf8c95452ff13971
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-04-12 18:46:04 +0200

    remove files

--- src/app/(protected)/server/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/auth";
-import { UserInfo } from "@/components/user-info";
-
-export default async function ServerPage() {
-  const session = await auth();
-
-  return <UserInfo label="Server Component" user={session?.user} />;
-}
