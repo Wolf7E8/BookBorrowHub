b265decb49920533a31ff4b094215f7fbe35988b
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-04-12 18:52:28 +0200

    style: change look of header for login/register forms

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -1,10 +1,5 @@
-import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"],
-});
+import { Book } from "lucide-react";
+import Link from "next/link";
 
 type HeaderProps = {
   label: string;
@@ -13,7 +8,12 @@ type HeaderProps = {
 export function Header({ label }: HeaderProps) {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+      <Link href="/" className="flex gap-x-2 items-center cursor-pointer">
+        <Book className="w-7 h-7  text-indigo-500 " />
+        <h1 className="font-bold text-xl text-gradient bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:underline decoration-indigo-500">
+          BookBorrowHub
+        </h1>
+      </Link>
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
   );
