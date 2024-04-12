1e0c2d95aa08ea5ac2f54b80aa45d33d8912b124
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-04-12 18:44:23 +0200

    fix: remove everything connected with user role

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,8 +1,6 @@
-import { UserRole } from "@prisma/client";
 import NextAuth, { DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
-  role: UserRole;
   isOAuth: boolean;
 };
 
